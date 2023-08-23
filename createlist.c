d8119f67d88a904dadca847314d6b6fa721c3e9c
YUGANDHAR K <114409737+Yugandhar2k1@users.noreply.github.com>
2023-08-23 23:54:00 +0530

    Add files via upload

---
+++ createlist.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+    int data;
+    struct node *next;
+};
+void PrintLinkedList(struct node *p)
+{
+    printf("Linked list elements are: \n");
+    while (p != NULL)
+    {
+        printf("%d ---> ", p ->data);
+        p = p -> data; 
+    }
+    printf("NULL");   
+}
+void main()
+{
+    struct node *head = NULL;
+    int node;
+    printf("Enter the number of nodes you want to create: ");
+    scanf("%d", &node);
+
+    for(int i=0; i<= node; i++)
+    {
+        int data;
+        printf("enter the data value for node %d: ", i+1);
+        scanf("%d", &data);
+
+        struct node *newnode = malloc(sizeof(struct node));
+        if(head == NULL)
+        {
+            head = newnode;
+        }
+        else{
+            struct node *current = head;
+            while(current->next != NULL)
+            {
+                current = current -> next;
+            }
+            current -> next = newnode;
+        }
+    }
+    PrintLinkedList(head);
+}
