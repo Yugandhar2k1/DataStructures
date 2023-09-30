5f6746d232916e8406aa69452e2fa2298e1dd169
YUGANDHAR K <114409737+Yugandhar2k1@users.noreply.github.com>
2023-09-30 10:56:43 +0530

    Add files via upload

---
+++ Single LinkedList Manual input.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+    int data;
+    struct node *next;
+};
+void printlist(struct node* p)
+{
+    while(p!=NULL)
+    {
+        printf("%d--->",p ->data);
+        p = p->next;
+    }
+    printf("NULL");
+}
+int main()
+{
+    struct node *head = NULL;
+    struct node *one;
+    struct node *two;
+    struct node *three;
+    struct node *four;
+    
+    one = malloc(sizeof(struct node));
+    two = malloc(sizeof(struct node));
+    three = malloc(sizeof(struct node));
+    four = malloc(sizeof(struct node));
+    
+    one->data= 12;
+    two->data=13;
+    three->data=14;
+    four->data=15;
+    
+    one->next=two;
+    two->next=three;
+    three->next=four;
+    four->next = NULL;
+    
+    head = one;
+    printlist(head);
+    return 0;
+}
